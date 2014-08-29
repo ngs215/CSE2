61eff63784d1d4a2301289bd700289e3e96797b7
ngs215 <ngs215@lehigh.edu>
2014-08-29 21:44:27 +0000

    Homework 01 Program following structure learned in lab one

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Nicholas Schan
+//hw01
+//Welcome Class Program
+//
+//define class
+
+public class WelcomeClass{
+    
+    //  add main method
+    public static void main(String[] args)  {
+    
+        // Print Welcome with Lehigh ID as signature
+    
+        System.out.println("       ----------- ");
+        System.out.println("        | WELCOME | ");
+        System.out.println("       ----------- ");
+        System.out.println("  ^   ^   ^   ^   ^   ^ ");
+        System.out.println(" ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ");
+        System.out.println("|-N- -G- -S- -2- -1- -5- | ");
+        System.out.println(" v v v v v v v v v v v v");
+        System.out.println("  v   v   v   v   v   v ");
+    }
+}
